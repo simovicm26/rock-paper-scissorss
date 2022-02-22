cb4837a439e05c1bef0d60490f3fb9a53636f3a5
Marko <simovicm26@gmail.com>
2022-02-22 08:16:18 +0100

    add player selection variable and check validity

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ function computerFunction (){
     var max = compSelection.length - 0.00001;
     return compSelection[Math.floor(Math.random()*max)];
 }
+//declare a function play game which takes inputs computerSelection and playerSelection and check their relationship
+//after that it returns 1 of 3 messages. 
+//"Its a tie try again!" or "You wont congratulations" or "You lost what a bummer"
+
+//we need to declare a variable called playerSelection that contains user input
+var playerSelection = prompt('Choose what you are going to play. Type rock, paper or scissors: ');
+
+//we need to check if the input is valid and if it is we are going to run the function play game
+if (!(playerSelection.toLowerCase() === 'rock' || playerSelection.toLowerCase() === 'scissors' || playerSelection.toLowerCase() === 'paper')){
+    alert('Write a valid input or check your spelling please.')
+} else 
